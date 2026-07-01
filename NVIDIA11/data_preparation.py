866b56e2a0d8781d4326938442383ac78ab7285c
Lorena Torres Jiménez <100475834@alumnos.uc3m.es>
2026-07-01 16:24:47 +0200

    Add files via upload

---
+++ NVIDIA11/data_preparation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+df = pd.read_csv('data/temp_database1.csv', sep=';')
+
+#Data preprocessing
+
+#Encoding of the target variable (if a patient has cancer or not: 1 if YES; O if NO)
+df['case_csPCa'] = df['case_csPCa'].map({'YES': 1, 'NO': 0})
+
+#Drop columns that are not important (dates, type of analysis...)
+cols_to_keep = ['patient_age', 'psa', 'psad', 'prostate_volume','case_csPCa']
+df = df[cols_to_keep]
+
+#Adjusting the psa column
+df = df[df['psa'] < 1000]
+
+# Mean imputation for filling NAs (median imputation may be also applied if outliers)
+df = df.fillna(df.median())
+
+
+#No-IID division
+df_ordenado = df.sort_values(by='psa')
+total_rows = len(df_ordenado)
+corte_1 = int(total_rows * 0.80)
+corte_2 = int(total_rows * 0.90)
+
+df_client1 = df_ordenado.iloc[:corte_1]           # 80% (Large hospital)
+df_client2 = df_ordenado.iloc[corte_1:corte_2]    # 10% (Small health center A)
+df_client3 = df_ordenado.iloc[corte_2:]           # 10% (Small health center B)
+
+
+df_client1.to_csv("data/client1.csv", sep=';', index=False)
+df_client2.to_csv("data/client2.csv", sep=';', index=False)
+df_client3.to_csv("data/client3.csv", sep=';', index=False)
+
+print("Datasets were generated correctly")
---
+++ NVIDIA20/my_tfm_job/app/custom/model.py
@@ -0,0 +1,15 @@
+﻿import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_model(input_dim):
+    tf.random.set_seed(42)
+    model = models.Sequential([
+        layers.Dense(64, activation='relu', input_shape=(input_dim,)),
+        layers.Dropout(0.2),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.2),
+        layers.Dense(16, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
